08b3068a6a6af56f87be470fa565227252173afe
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-10-18 14:04:05 +0000

    Creating database in MySQL Server

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,5 @@
 import os
 import mysql.connector
-from mysql.connector import Error
 
 try:
     connection = mysql.connector.connect(
@@ -15,8 +14,8 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created  successfully!")
 
-except Error as e:
-    print(f"Error: {e}")
+except mysql.connector.Error as e:
+    print(f"Database Error: {e}")
 
 finally:
     if connection.is_connected():
